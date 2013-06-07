effb1ec2caa462fcce7d240688bc9c599702145f
Matthew Reidsma <matthew@reidsrow.com>
2013-06-07 12:26:57 -0400

    Swapped out yellow checkmark for dash, added alt tags to status images, optimized pngs for faster loading

--- index.php
+++ index.php
@@ -182,7 +182,7 @@
 									$cnt = 0;
 									foreach(range(0,5) as $cnt) {
 										echo '<td style="text-align: center">';
-										echo '<img src="resources/img/checkmark.png">';
+										echo '<img alt="' . $rw['status_type_text'] . 'src="resources/img/checkmark.png">';
 										echo '</td>';
 									}
 
@@ -196,7 +196,7 @@
 
 										echo'<td style="text-align: center">';
 
-										$day_status = '<img src="resources/img/checkmark.png">';
+										$day_status = '<img  alt="' . $rw['status_type_text'] . ' src="resources/img/checkmark.png">';
 
 										$system_result = $db->query ("SELECT i.start_time, i.end_time, i.status_type_id
 																FROM issue_entries i
@@ -214,11 +214,11 @@
 												echo '<a href="detail.php?system_id='. $row['system_id'] .'&day='. ($day-$cnt) .'" style = "text-decoration: none;">';
 
 												if ($rw['status_type_id'] == 2) {
-													$day_status = '<b style= "color: red">X</b>';
+													$day_status = '<b style= "color: red" title="' . $rw['status_type_text'] . '"">X</b>';
 												}
 
 												else {
-													$day_status = '<b style= "color: orange">---</b>';
+													$day_status = '<img  alt="' . $rw['status_type_text'] . ' src="resources/img/checkmark-yellow.png">';
 												}
 											}
 										}
