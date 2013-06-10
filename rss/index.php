436b6b8ba7d1b0bc143d046c5f8bbe7f8fed5417
Matthew Reidsma <matthew@reidsrow.com>
2013-06-10 16:18:55 -0400

    Fixed typo in object in RSS item echo loop

--- rss/index.php
+++ rss/index.php
@@ -47,7 +47,7 @@ $result = $db->query("SELECT s.status_id, s.status_id, s.status_timestamp, s.sta
 echo '<item> 
 <title>' . $obj->status_type_text . ' for ' . $obj->system_name . '</title>
 <link>http://labs.library.gvsu.edu/status/detail.php?system_id=' . $obj->system_id . '&amp;day=' . date("Ymd", $obj->status_timestamp) . '</link>
-<description><![CDATA[' . Markdown($obj->status_text) . ']]></description>
+<description><![CDATA[' . $obj->status_text . ']]></description>
 <author>' . $obj->user_email . ' (' . $obj->user_fn . ' ' . $obj->user_ln . ')</author>
 <pubDate>' . date('D, d M Y g:i:s O', $obj->status_timestamp) . '</pubDate>
 <guid>http://labs.library.gvsu.edu/status/detail.php?system_id=' . $obj->system_id . '&amp;day=' . date("Ymd", $obj->status_timestamp) . '</guid> 
