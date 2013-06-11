8ed786b696800616505f8b7e14a8102dbac7fe91
Matthew Reidsma <reidsmam@gvsu.edu>
2013-06-11 11:05:50 -0400

    Added dummy config file

---
+++ resources/config-sample.php
@@ -0,0 +1,10 @@
+<?php
+
+// Save this as /resources/secret/config.php
+
+// You probably also want to make these declarations with DEFINE or something.
+
+$db_host = ''; // Database host
+$db_user = ''; // Database user
+$db_pass = ''; // Database password
+$db_database = ''; // Database name
