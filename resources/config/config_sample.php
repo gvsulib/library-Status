bbcf204b1201ab6bcd79c1f2c70a47e4889907b7
kyle felker <felkerk@gvsu.edu>
2017-11-28 14:02:16 -0500

    adding config sample

---
+++ resources/config/config_sample.php
@@ -0,0 +1,36 @@
+<?php
+
+// Save this as /resources/secret/config.php
+
+$db_host = ''; // Database host
+
+$db_user = ''; // Database user
+$db_pass = ''; // Database password
+$db_database = ''; // Database name
+
+// Styles
+$library_name = 'Library';
+$header_image = 'resources/img/sample_logo.png';
+$header_url = '';
+$banner_color = '#333';
+
+// Subscriptions
+$rss_url = '';
+$email_subscription_url = '';
+
+// Set default variables for sending emails
+$to_email = '';
+$from_email = '';
+$email_subject = '';
+$library_phone = '';
+
+//Recaptcha API Keys
+$recaptchaSiteKey = '';
+$recaptchaSecretKey = '';
+
+
+//Native login or not
+$use_native_login = false;
+$non_native_login_url = '';
+
+?>
